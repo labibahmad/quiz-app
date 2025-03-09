ec4697e7a9ea87543a4da7b9c7fcd743d50b51d2
Ahmad <labibahmad136@gmail.com>
2025-03-10 01:09:50 +0600

    updated small code

--- app.js
+++ app.js
@@ -5,5 +5,22 @@ const questions = [
         possibleAnswers: ['blue', 'green'],
         correctAnswer: 'blue'
 
+    },
+    {
+        topic: '',
+        question: '',
+        possibleAnswers: ['blue', 'green'],
+        correctAnswer: 'blue'
+
     }
 ]
+
+const quizProgress = document.getElementById('quizProgress')
+const questionContainer = document.getElementById('questionContainer')
+const answerContainer = document.getElementById('answerContainer')
+
+function handleQuestion(){
+    questions.forEach((question) => {
+        quizProgress.innerHTML += "<span></span>";
+    });
+}
