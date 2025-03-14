ec2a376e1756976d8c927a29faaab47388edd11b
Ahmad <labibahmad136@gmail.com>
2025-03-15 01:12:53 +0600

    updated button functions

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 const questions = [
     {
-        topic: '',
-        question: '',
-        possibleAnswers: ['blue', 'green'],
-        correctAnswer: 'blue'
+        topic: 'IQ',
+        question: 'What color is the sun?',
+        possibleAnswers: ['white', 'yellow'],
+        correctAnswer: 'white'
 
     },
     {
-        topic: '',
-        question: '',
-        possibleAnswers: ['blue', 'green'],
-        correctAnswer: 'blue'
+        topic: 'Anime',
+        question: 'Greatest anime ever?',
+        possibleAnswers: ['AOT', 'Deathnote'],
+        correctAnswer: 'AOT'
 
     }
 ]
 
-const quizProgress = document.getElementById('quizProgress')
-const questionContainer = document.getElementById('questionContainer')
-const answerContainer = document.getElementById('answerContainer')
+const quizProgress = document.getElementById("quizProgress");
+const questionContainer = document.getElementById("questionContainer");
+const answerContainer = document.getElementById("answerContainer");
 
-function handleQuestion(){
+let currentQuestionIndex = 0;
+
+function handleQuestion(index) {
     questions.forEach((question) => {
         quizProgress.innerHTML += "<span></span>";
     });
-}
+
+    let spans = document.querySelectorAll("span");
+    for (let i=0; i<= index; i++){
+        spans[i].classList.add("seen");
+    }
+
+    //topics/questions
+    questionContainer.innerHTML = 
+    `<p>${questions[index].topic}</p>
+    <p>${questions[index].question}</p>`;
+
+    //ANSWERS
+    questions[index].possibleAnswers.forEach(answer => {
+        answerContainer.innerHTML += `<button>${answer}</button>`
+    });
+
+    let answers = document.querySelectorAll("button");
+    answers.forEach((answer)=>{
+        answer.addEventListener("click", (e) => {
+            if(e.target.textContent === questions[index].correctAnswer) {
+                console.log("correct");
+            } else {
+                console.log("wrong");
+            }
+            });
+        });
+
+        
+
+    };
+
+handleQuestion(currentQuestionIndex);
